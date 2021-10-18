6fb4aaf46dccbc2e57a0b11406c263440abd4b75
Luis NIETO MUNOZ <lnieto-m@student.42.fr>
2021-10-18 21:10:51 +0200

    added tldr and removed lorem ipsum

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import React, { useRef, useEffect, useState, ReactElement } from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
-import MyDropZone from './component/customDropzoen';
-import { gsap } from 'gsap';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import { PopupActions } from 'reactjs-popup/dist/types';
 import Toggle from 'react-toggle';
 import "react-toggle/style.css"
-import { FaSun, FaMoon } from 'react-icons/fa';
+import { FaMoon } from 'react-icons/fa';
 import { useMediaQuery } from 'react-responsive';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Linkedin, Github, Sun } from 'react-bootstrap-icons';
+import { Briefcase, Globe, Sun } from 'react-bootstrap-icons';
  
 function App() {
 
@@ -43,12 +40,8 @@ function App() {
     }
   }
 
-  const onClick = (e: React.MouseEventHandler<HTMLButtonElement>) => {
-    requestServer();
-  }
-
   const requestServer = async () => {
-    if (currentLink == '') {
+    if (currentLink === '') {
       return;
     }
     setLoading(true)
@@ -82,7 +75,7 @@ function App() {
     <div className="App">
       {console.log('render', loading)}
       {loading
-        ? <div className="overlay"><img src={'https://media3.giphy.com/media/IQebREsGFRXmo/200.gif'}/></div>
+        ? <div className="overlay"><img alt="Loading Mew" src={'https://media3.giphy.com/media/IQebREsGFRXmo/200.gif'}/></div>
         : <div></div>
       }
 
@@ -92,14 +85,14 @@ function App() {
         className="response-popup"
         ref={ref}>
         <div id="response-popup-content">
-          <img className="pikachu" src={currentLink} />
+          <img alt="Ton pokemon fourni" className="pikachu" src={currentLink} />
           <p style={{ margin: '1em' }}> {serverResponse} </p>
         </div>
       </Popup>
       <div className="content">
         <div className="header">
           <div>
-            <img className="pikachu" src={'/poke-front/pikachu.png'} />
+            <img alt="Detective Pikachu" className="pikachu" src={'/poke-front/pikachu.png'} />
           </div>
           <div className="toggle-position">
             <Toggle
@@ -120,15 +113,41 @@ function App() {
           }}/>
           <Button variant="outline-primary" disabled={loading} onClick={(e) => { if (!loading) { requestServer(); }}}> {loading ? 'Loading...' : 'Go' } </Button>{' '}
         </div>
+        <div className="tldr">
+          <article>
+            <h2>Comment ça marche ? :</h2>
+            <p>
+              Copier l'URL de l'image d'un Pokémon de la 1re génération (Pokémon n°1 - Bulbizarre à 151 - Mew) dans le champ de recherche et cliquer sur Go.<br />
+              L'image doit être "propre", c'est-à-dire qu'elle ne doit pas contenir trop de bruit visuel et que le Pokémon doit être seul et dans sa couleur d'origine.<br />
+              <br />
+              Ensuite, un serveur en Python va récupérer l'image partagée, la redimensionner et la transformer en tableau. Le réseau de neurones, qui a été entraîné avec environ 12 000 images, va réaliser une analyse couche par couche. Il va identifier les caractéristiques propres à ce Pokémon et les comparer à celles apprises pendant son entraînement pour donner en retour le nom d'un Pokémon et la probabilité que ce soit lui.<br />
+            </p>
+          </article>
+          <article>
+            <h2>Mise en garde :</h2>
+            <p>
+              - Le set d'image a été construit à partir de plusieurs sets différents, qui ont été nettoyés pour récupérer les images les plus fidèles.<br />
+              - Il s'agit d'un projet de démo, c'est pourquoi il ne contient que la 1re génération de Pokémon.<br />
+              - La probabilité de reconnaître un Pokémon dépends du nombre d'images le représentant dans le set d'entraînement (pkmn plus populaire = plus d'image d'entraînement).<br />
+            </p>
+          </article>
+          <article>
+            <h2>Ce qui fait tourner le Pokédex :</h2>
+            <p>
+            - Python, TensorFlow et Keras (Librairie), Docker (Containers : 1 pour le réseau de neurones, 1 pour le serveur), GitHub, Heroku (hébergement)
+            </p>
+          </article>
+        </div>
+
       </div>
 
       <footer className="App-footer">
         <div className="links">
-          <a href="#"><Linkedin /></a>
-          <a href="#"><Github /></a>
+          <a href="https://www.malt.fr/profile/oxpure"><Briefcase /></a>
+          <a href="https://oxpure.tech/"><Globe /></a>
         </div>
-        <p style={{ paddingTop: '0.6em' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-        <p className="copyright">©2021 - Morbi tincidunt magna leo</p>
+        <p style={{ paddingTop: '0.6em' }}>Pokémon c'était pas mieux avant</p>
+        <p className="copyright">©2021 - Frontpage made by <a className="linkMaker" href="https://github.com/lnieto-m"> Luis Nieto Munoz </a></p>
       </footer>
     </div>
   );
